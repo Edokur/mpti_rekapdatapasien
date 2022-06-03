519ddd8c124282af8ec6489ba129f408c08d2078
alviahtzkhh <alviahtu05@gmail.com>
2022-06-03 13:30:50 +0700

    mpti

---
+++ alvia/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="container">
+		<form action="" method="POST" class="login-email" >
+			<p class="login-text" style="font-size: 2rem; ">Selamat Datang...</p>
+			<h5>Mohon masukkan email dan kata sandi anda</h5><br>
+			<div class="input-group">
+				<input type="email" placeholder="Masukkan e-mail anda" name="email" required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Masukkan kata sandi anda" name="password" required>
+			</div>
+			<div class="code">
+				<table>
+				<tr>
+				<td><h3> kode </h3></td>
+				<td><input placeholder="Masukkan captcha" type='text' class="input-code"><td>
+				</tr>
+			</table>
+			</div>	
+            <a href="#">Forgot Password?</a>
+			<div class="input-group">
+				<button name="submit" class="button">Login</button>
+			</div>
+		</form>
+	</div>
+	</div></div>
+	<footer>
+		<p class="foot-sistem">Sistem Rekapitulasi Data</p>
+		<p class="foot-created">Created by Problem Solver</p>	
+	</footer>
+</body>
+</html>
