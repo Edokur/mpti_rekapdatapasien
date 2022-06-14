a23545478fa5f8e982f28b70a047b2b3d505f165
jangkungpangestuaji <ajijangkung7.com>
2022-06-14 21:02:13 +0700

    reupload sidebar

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -14,6 +14,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
 </head>
+
 <body>
     <div class="side-bar my-3">
         <div class="menu mt-5">
