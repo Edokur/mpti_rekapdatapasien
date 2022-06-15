084c1c9246c9faec521c3c6e7637a04d4088c993
sendyapriatna <sendyapriatna001@gmail.com>
2022-06-15 11:33:04 +0700

    Menambah Model Identitas

---
+++ app/Models/Identitas.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Identitas extends Model
+{
+    use HasFactory;
+
+    protected $table = 'identitas_pasien';
+
+    protected $fillable = [
+        'id',
+        'nama_pasien',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'alamat',
+        'kepala_keluarga',
+        'no_bpjs',
+    ];
+}
