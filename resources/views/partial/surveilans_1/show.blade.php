7b565285885921d44419f8d14b2c8b282c0a635a
jangkungpangestuaji <ajijangkung7.com>
2022-07-22 17:30:44 +0700

    Surveilans1

--- resources/views/partial/surveilans_1/show.blade.php
+++ resources/views/partial/surveilans_1/show.blade.php
@@ -15,10 +15,16 @@
                 <h4 class="text-title color-black lead font-bold">Surveilans 1</h4>
                 <h1 class="color-neutral-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porttitor adipiscing in consectetur sem ut netus scelerisque. Viverra urna vitae viverra habitant a magna vitae, fermentum morbi. Magna magna non ridiculus vitae viverra feugiat morbi sed.</h1>
             </div>
-            <div class="ttable border-line bg-white border-radius mt-4">
+            <div class="row mt-5">
+                <div class="col">
+                    <a href="/surveilans_1" class="btn btn-purple text-white px-5 py-3">Kembali</a>
+                </div>
+            </div>
+            <div class="ttable border-line bg-white my-4">
                 <div class="p-2 bg-light rounded-bottom-0">
                     <p class="m-3 text-dark">Detail Data</p>
                 </div>
+                
                 <div class="text-dark border-top">
                     <form class="ml-5 p-5" method="POST" action="/surveilans_1">
                         <div class="form-group py-2 row">
@@ -68,11 +74,6 @@
                             <div class="col-sm-8">
                                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" disabled>{{$data->diagnosa}}</textarea>
                             </div>
-                        </div>
-
-                        <div class="form-group row mx-1 py-2">
-                            <label for="" class="col-3"></label>
-                            <a href="/surveilans_1" type="button" class="col-4 py-3 mr-1 btn btn-outline-danger  ">Kembali</a>
                         </div>
                     </form>
                 </div>
