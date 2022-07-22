4b0db94f2e11504e62f53ee050a8d2c3e733c7ce
Sendy Apriatna <sendyapriatna001@gmail.com>
2022-07-22 13:54:40 +0700

    DB Migrasi

--- database/migrations/2022_06_24_124655_create_log_activity_table.php
+++ database/migrations/2022_06_24_124655_create_log_activity_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLogIdentitasPasienTable extends Migration
+class CreateLogActivityTable extends Migration
 {
     /**
      * Run the migrations.
